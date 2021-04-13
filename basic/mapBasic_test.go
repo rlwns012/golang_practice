712b58d8797c466d10e59b7df2af9509f4432ad6
rlwns012 <rlwns012@gmail.com>
2021-04-13 21:05:14 +0900

    golang test 실행

--- basic/mapBasic_test.go
+++ basic/mapBasic_test.go
@@ -1,7 +1,13 @@
 package basic
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func Test(t *testing.T) {
 	Hi()
+	// assert.Equal(t, 123, 125, "they should be equal")
+	assert.NotEqual(t, 123, 123, "they should not be equal")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module practice
 
 go 1.13
+
+require github.com/stretchr/testify v1.7.0
